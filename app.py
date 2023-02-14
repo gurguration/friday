f089a894aad59d10cd4083f51df3a7d13457146f
gurguration <g.zakareishvili@gmail.com>
2023-02-14 16:04:27 +0400

    Add flask app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def is_it_friday():
+    today = datetime.datetime.now().strftime('%A')
+    if today == 'Friday':
+        return "It is friday !\n"
+    else:
+        return "It is not friday yet :(\n"
+
+if __name__ == '__main__':
+    app.run(port=5000)
